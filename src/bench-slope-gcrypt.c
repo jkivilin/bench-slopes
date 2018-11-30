7473d85f60983bb467870ff4b1fbc407db347c41
Jussi Kivilinna <jussi.kivilinna@iki.fi>
2018-11-30 19:57:39 +0200

    Update copyright headers (project name and copyright year)
    Signed-off-by: Jussi Kivilinna <jussi.kivilinna@iki.fi>

--- src/bench-slope-gcrypt.c
+++ src/bench-slope-gcrypt.c
@@ -1,14 +1,14 @@
 /* bench-slope-gcrypt.c - for libgcrypt
- * Copyright (C) 2013,2017 Jussi Kivilinna <jussi.kivilinna@iki.fi>
+ * Copyright (C) 2013,2017-2018 Jussi Kivilinna <jussi.kivilinna@iki.fi>
  *
- * This file is part of Libgcrypt.
+ * This file is part of Bench-slopes.
  *
- * Libgcrypt is free software; you can redistribute it and/or modify
+ * Bench-slopes is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser general Public License as
  * published by the Free Software Foundation; either version 2.1 of
  * the License, or (at your option) any later version.
  *
- * Libgcrypt is distributed in the hope that it will be useful,
+ * Bench-slopes is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
